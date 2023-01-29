64c3ce51092a2d6509a3a83a9d4d2a1f144f735d
Arun Kumar <dahiya.akd94@gmail.com>
2023-01-29 12:31:13 +0530

    Initial Commit

---
+++ src/main/java/com/java/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,24 @@
+package com.java.moviecatalogservice.resources;
+
+import com.java.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        return Collections.singletonList(
+                new CatalogItem("Iron Man", "Action", 4)
+        );
+
+    }
+
+}
---
+++ src/test/java/com/java/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
