bc591f64f881234ff3a916cf31f1b7dede5fe1f3
Arun Kumar <dahiya.akd94@gmail.com>
2023-01-30 18:46:24 +0530

    Fetching data from movie info service using rest call

--- src/main/java/com/java/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/java/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,12 +2,20 @@ package com.java.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
+    /** this singleton instance will be shared across the application */
+	@Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
 
 }
--- src/main/java/com/java/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/java/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,23 +1,52 @@
 package com.java.moviecatalogservice.resources;
 
 import com.java.moviecatalogservice.models.CatalogItem;
+import com.java.moviecatalogservice.models.Movie;
+import com.java.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    /** @Autowired is the consumer saying I need this dependency whereas
+     * @Bean is the producer saying I will be needed */
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        return Collections.singletonList(
-                new CatalogItem("Iron Man", "Action", 4)
+        // Get all rated movie Ids
+        /** This part will be taken care of by calling Rating info microservice to avoid hardcoding */
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123", 4),
+                new Rating("124", 3)
         );
+
+        /** Url should be dynamic to avoid issues */
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Action", rating.getRating());
+        }).collect(Collectors.toList());
+
+        // For each movie Id, call movie service to get the details
+
+        // Put them all together
+
+        /** Hard coded in initial step to just return a simple object */
+/*        return Collections.singletonList(
+                new CatalogItem("Iron Man", "Action", 4)
+        );*/
 
     }
 
