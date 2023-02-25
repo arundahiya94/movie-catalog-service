df2d5f0d1e23f3ca2c8b2c3fd67ffb509800c7ea
Arun Kumar <dahiya.akd94@gmail.com>
2023-02-25 10:52:43 +0530

    Added Dependency for Webclient and comments

--- src/main/java/com/java/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/java/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,6 +24,9 @@ public class MovieCatalogResource {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -35,7 +39,16 @@ public class MovieCatalogResource {
 
         /** Url should be dynamic to avoid issues */
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+             /** Use of Webclient instead of RestTemplate */
+            /* Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();*/
+
             return new CatalogItem(movie.getName(), "Action", rating.getRating());
         }).collect(Collectors.toList());
 
